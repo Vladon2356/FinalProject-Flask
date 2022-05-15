d149581cf6fcea58841d7096e7b1b875905bd1bb
VladOnischuk <vlad.onischuk1234@gmail.com>
2022-05-15 19:55:17 +0300

    Add run.py

---
+++ cinema/views/__init__.py
@@ -0,0 +1,6 @@
+from .users import users_bp
+from .halls import halls_bp
+from .auth import auth_bp
+from .sessions import sessions_bp
+from .movies import movies_bp
+from .tickets import tickets_bp
